95f4a35b8bacd4c0d56c409777558c5d16ce6cd9
mengdaoshizhongxinyang <729403918@qq.com>
2021-01-20 18:01:25 +0800

    chore:add ws-mock

---
+++ ws-mock.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: mengdaoshizhongxinyang
+ * @Date: 2021-01-20 14:01:30
+ * @Description: 
+ * @GitHub: https://github.com/mengdaoshizhongxinyang
+ */
+require('./ws-mock')
---
+++ ws-mock/index.js
@@ -0,0 +1,41 @@
+/*
+ * @Author: mengdaoshizhongxinyang
+ * @Date: 2021-01-20 14:01:17
+ * @Description: 
+ * @GitHub: https://github.com/mengdaoshizhongxinyang
+ */
+const WebSocket = require('ws');
+
+const server = new WebSocket.Server({ port: 8080 });
+
+server.on('open', function open() {
+  console.log('connected');
+});
+
+server.on('close', function close() {
+  console.log('disconnected');
+});
+
+server.on('connection', function connection(ws, req) {
+  const ip = req.socket.remoteAddress;
+  const port = req.socket.remotePort;
+  const clientName = ip + port;
+
+  console.log('%s is connected', clientName)
+
+  // 发送欢迎信息给客户端
+  ws.send("Welcome " + clientName);
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s from %s', message, clientName);
+    
+    // 广播消息给所有客户端
+    server.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send( clientName + " -> " + message);
+      }
+    });
+
+  });
+
+});
